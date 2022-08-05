a3cc9c990541c017ee17e4ac231571beb6bf8d21
Muhammet Çağatay <muhammet.cagatay.mc@gmail.com>
2022-08-05 10:24:00 +0300

    applied options pattern

---
+++ src/Movie/Movie.API/Models/Settings/IMongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Movie.API.Models.Settings
+{
+    public interface IMongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ src/Movie/Movie.API/Models/Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Movie.API.Models.Settings
+{
+    public class MongoDbSettings : IMongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
